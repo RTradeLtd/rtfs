f1017b06ae840fd73f6615c21e3759e87c1fd898
potsables <postables@rtradetechnologies.com>
2018-11-23 00:03:44 -0800

    chore: update gopkg, add a comment

--- krab/utils.go
+++ krab/utils.go
@@ -1,19 +1,20 @@
 package krab
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/pkg/errors"
 )
 
+// all of this is taken from https://github.com/ipfs/go-ipfs-keystore
+
 var (
 	// ErrNoSuchKey is returned if a key of the given name is not found in the store
-	ErrNoSuchKey = fmt.Errorf("no key by the given name was found")
+	ErrNoSuchKey = errors.New("no key by the given name was found")
 	// ErrKeyExists is returned when writing a key would overwrite an existing key
-	ErrKeyExists = fmt.Errorf("key by that name already exists, refusing to overwrite")
+	ErrKeyExists = errors.New("key by that name already exists, refusing to overwrite")
 	// ErrKeyFmt is returned when the key's format is invalid
-	ErrKeyFmt = fmt.Errorf("key has invalid format")
+	ErrKeyFmt = errors.New("key has invalid format")
 )
 
 func validateName(name string) error {
