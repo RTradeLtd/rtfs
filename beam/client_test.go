252a4ce04d5664341c4353afb3192b05d1b408b2
potsables <postables@rtradetechnologies.com>
2018-11-23 23:07:40 -0800

    beam: add client test

---
+++ beam/client_test.go
@@ -0,0 +1,39 @@
+package beam_test
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/RTradeLtd/config"
+	"github.com/RTradeLtd/rtfs"
+	"github.com/RTradeLtd/rtfs/beam"
+)
+
+func TestBeam(t *testing.T) {
+	cfg, err := config.LoadConfig("../testenv/config.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	laser, err := beam.NewLaser(cfg.IPFS, cfg.IPFS)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rtfsManager, err := rtfs.NewManager(cfg.IPFS.APIConnection.Host+":"+cfg.IPFS.APIConnection.Port, nil, time.Minute*5)
+	if err != nil {
+		t.Fatal(err)
+	}
+	file, err := os.Open("../testenv/config.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	cid, err := rtfsManager.Add(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i := 1; i <= 2; i++ {
+		if err = laser.Beam(i, cid); err != nil {
+			t.Fatal(err)
+		}
+	}
+}
