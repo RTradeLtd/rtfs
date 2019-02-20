3ae36e7d4ee9ef34e91c95ccc6c849098136c8b6
potsables <postables@rtradetechnologies.com>
2019-02-20 15:13:44 -0800

    rtfs: fix unterminated string

--- rtfs_test.go
+++ rtfs_test.go
@@ -22,7 +22,7 @@ const (
 	testRefsHash        = "QmPS6VssQGyBYjGQSK8ordvXaU1yUoaUmTfmrV7daLeRPH"
 	nodeOneAPIAddr      = "192.168.1.101:5001"
 	nodeTwoAPIAddr      = "192.168.2.101:5001"
-	remoteNodeMultiAddr = "/ip4/172.218.49.115/tcp/4003/ipfs/QmXow5Vu8YXqvabkptQ7HddvNPpbLhXzmmU53yPCM54EQa
+	remoteNodeMultiAddr = "/ip4/172.218.49.115/tcp/4003/ipfs/QmXow5Vu8YXqvabkptQ7HddvNPpbLhXzmmU53yPCM54EQa"
 )
 
 type args struct {
