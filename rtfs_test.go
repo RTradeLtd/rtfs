4575ae159ad3ce350062fecb7cf390b4af043bdb
potsables <postables@rtradetechnologies.com>
2019-03-07 23:04:24 -0800

    chore: add log print

--- rtfs_test.go
+++ rtfs_test.go
@@ -3,6 +3,7 @@ package rtfs_test
 import (
 	"context"
 	"encoding/json"
+	"log"
 	"os"
 	"reflect"
 	"sort"
@@ -233,6 +234,7 @@ func TestAdd(t *testing.T) {
 			if resp, err := im.Add(file); err != nil {
 				t.Fatal(err)
 			} else if resp != "QmSwK5Mw7YHP69gF6oYNZdsuuh37HR3SSiUxu3F5fXZ3az" {
+				log.Print(resp)
 				t.Fatal("bad hash generated")
 			}
 		})
